f01196939003ba4930cbfe89c618ce0f76d16f6f
Sudeep Biswas <w3bdev@protonmail.com>
2024-02-24 16:00:54 +0530

    overconfidence :( no token from NextToken()

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "github.com/w3bdev1/bodmas/token"
+
 type Lexer struct {
 	Content      string
 	NextPosition int
@@ -23,4 +25,45 @@ func (l *Lexer) ReadChar() {
 		l.Char = l.Content[l.NextPosition]
 		l.NextPosition += 1
 	}
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+	switch l.Char {
+	case '+':
+		tok = createToken(token.ADD, l.Char)
+	case '-':
+		tok = createToken(token.SUBTRACT, l.Char)
+	case '*':
+		tok = createToken(token.MULTIPLY, l.Char)
+	case '/':
+		tok = createToken(token.DIVIDE, l.Char)
+	default:
+		if isDigit(l.Char) {
+			tok.Type = token.INT
+			tok.Value = l.getNum()
+			return tok
+		}
+	}
+
+	return tok
+}
+
+func createToken(tokenType string, value byte) token.Token {
+	return token.Token{Type: tokenType, Value: string(value)}
+}
+
+func isDigit(char byte) bool {
+	return char >= 0 && char <= 9
+}
+
+func (l *Lexer) getNum() string {
+
+	start_location := l.NextPosition - 1
+	for isDigit(l.Char) {
+		l.ReadChar()
+	}
+	end_location := l.NextPosition - 1
+
+	return l.Content[start_location:end_location]
 }
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -1,17 +1,54 @@
 package lexer_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/w3bdev1/bodmas/lexer"
+	"github.com/w3bdev1/bodmas/token"
 )
 
-func TestNewLexer(t *testing.T)  {
-  content := "abcd"
+func TestNewLexer(t *testing.T) {
+	content := "abcd"
 
-  l := lexer.NewLexer(content)
+	l := lexer.NewLexer(content)
 
-  if !(l.Content == content && l.NextPosition == 1 && l.Char == content[0]) {
-     t.Fatalf("NewLexer(%s) creation failed:", content)
-  }
+	if !(l.Content == content && l.NextPosition == 1 && l.Char == content[0]) {
+		t.Fatalf("NewLexer(%s) creation failed:", content)
+	}
+}
+
+func TestNextToken(t *testing.T) {
+	content := "20 + 4 * 3 / 6 - 1"
+
+	expectations := []struct {
+		expectedType  string
+		expectedValue string
+	}{
+		{token.INT, "20"},
+		{token.ADD, "+"},
+		{token.INT, "4"},
+		{token.MULTIPLY, "*"},
+		{token.INT, "3"},
+		{token.DIVIDE, "/"},
+		{token.INT, "6"},
+		{token.SUBTRACT, "-"},
+		{token.INT, "1"},
+	}
+
+	l := lexer.NewLexer(content)
+
+	for index, test := range expectations {
+		tok := l.NextToken()
+		fmt.Println(test)
+		fmt.Println(tok)
+
+		if tok.Type != test.expectedType {
+			t.Fatalf("Test[%d] failed: expected type %s, got %s", index, test.expectedType, tok.Type)
+		}
+
+		if tok.Value != test.expectedValue {
+			t.Fatalf("Test[%d] failed: expected value %s, got %s", index, test.expectedValue, tok.Value)
+		}
+	}
 }
